55a2f6e96f99e2733c611826b840f3791e558fb3
michalszczepanski91 <michalszczepanski91@gmail.com>
2017-01-29 18:17:50 +0100

    new v

--- load_matrix_if_sentance.py
+++ load_matrix_if_sentance.py
@@ -11,14 +11,26 @@ if len(sys.argv) < 2:
 	print "..."
 	sys.exit(0)
 
-#where i am
-#basedir = os.path.abspath(os.path.dirname(__file__))
+## HERE DEFINE WHAT ARE YOU LOOKINg FOR ##
+typeOfFile = '*.txt'
+pathToFiles = sys.argv[1]
+pathToFiles = pathToFiles + typeOfFile
 
+files = glob.glob(pathToFiles)
+for input_file in files:
+	print "input file : "+ input_file
+	with open(input_file, 'r') as f:
+		first_line = f.readline()
+		howmuchRinfile = first_line
+		print "elements of R " + first_line
+		content = f.readlines()
+		content = [x.strip() for x in content]
+		print content
+		if content == "R":
+			print "wartosc:" 
+			print content
+sys.exit(0)
 
-#print (basedir)
-print (sys.argv)
-
-glob.glob(sys.argv/'*.txt')
 
 
 
